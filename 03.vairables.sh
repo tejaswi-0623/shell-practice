14b473dc992286f7115dacce0d25ea26bfdbb115
tejaswi <tejaswi0623@gmail.com>
2026-02-13 17:46:03 -0500

    message

---
+++ 03.vairables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$person1=honey
+$person2=mani
+
+echo "$person1 :: Hi $person2, how are you?"
+echo "$person2 :: hi $person1, doing good. what about you?"
+echo "$person1 :: i am also doing fine $person2, "what are you learning today""
+echo "$person2 :: " i am learning something today $person1
