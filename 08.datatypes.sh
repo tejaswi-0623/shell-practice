9c74215494826b16ce3eab77095ff81cfc229ee9
tejaswi <tejaswi0623@gmail.com>
2026-02-15 16:22:19 -0500

    msg

--- 08.datatypes.sh
+++ 08.datatypes.sh
@@ -5,11 +5,10 @@ num1=100
 
 #sum=$(($num1+num2))
 #echo "the result is: $sum"
-
 #syntax error: invalid arithmetic operator (error token is ".5") shell doesn't support float
 
 num3=200
 sum2=$(($num1+$num3))
-ech0 "the result is: $sum2"
+echo "the result is: $sum2"
 
 
