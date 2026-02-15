f65ab99661f476cdda3081d25263f7f0fbe03908
tejaswi <tejaswi0623@gmail.com>
2026-02-15 17:40:33 -0500

    message

--- 10.exitcode.sh
+++ 10.exitcode.sh
@@ -10,3 +10,16 @@ fi
 echo "installing nginx"
 dnf install nginx -y
 
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... FAILURE"
+    exit 1
+else
+    echo "Installing Nginx ... SUCCESS"
+fi
+
+
+#id -u represents user id if id is 0--->root access o
+#id is number---->user id
+#exit 1 means if any error then stop the script
+#sudo sh 10.exitcode.sh
+#nginx will be installed now
