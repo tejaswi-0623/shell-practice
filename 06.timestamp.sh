00cacabdf69fe25d47e6cf92931e5cf2751ebe18
tejaswi <tejaswi0623@gmail.com>
2026-02-15 15:30:12 -0500

    message

--- 06.timestamp.sh
+++ 06.timestamp.sh
@@ -1,5 +1,15 @@
 #!bin/bash
 
-timestamp=$(date)
+#timestamp=$(date)
+#echo "script executed at: $timestamp" ----to know executed script time
 
-echo "script executed at: $timestamp"
+start_time=$(date +%s)
+echo "program start time is: $start_time"
+
+sleep 10
+
+end_time=$(date +%s)
+echo "program executed time is: $end_time"
+
+total_time=(($end_time-$start_time))
+echo "the duration of program is: $total_time"
