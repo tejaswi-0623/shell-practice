a676437b5c581c261f04d2b3da5aab07d807d831
tejaswi <tejaswi0623@gmail.com>
2026-02-15 15:59:41 -0500

    mesage

--- 07.specialvairables.sh
+++ 07.specialvairables.sh
@@ -8,6 +8,6 @@ echo "to know script name: $0"
 echo "present working directory: $PWD"
 echo "who is running the script: $USER"
 echo "process intsance id (PID) of the script: $$"
-sleep 10
-echo "process instance id (PID) of recenelt excuted background process: $!"
+sleep 10 &
+echo "process instance id (PID) of recently excuted background process: $!"
 echo "exit status of previous command: $?"
