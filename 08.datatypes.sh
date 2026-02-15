8d98b6b95ff7e26b0b0c1689aead1a2e46b54848
tejaswi <tejaswi0623@gmail.com>
2026-02-15 16:32:04 -0500

    msg

--- 08.datatypes.sh
+++ 08.datatypes.sh
@@ -10,10 +10,19 @@ num1=100
 num3=200
 sum2=$(($num1+$num3))
 echo "the result is: $sum2"
+#the result is: 300
+
 
 
 num4=honey
 sum3=$(($num1+$num4))
 echo "the result is: $sum3"
+#the result is: 100 here it takes string value as zero and performed operation
 
-
+#Array
+fruits=("apple","cherry","banana","pear")
+echo "Fruits we defined are: ${fruits[@]}"
+echo "first fruit is: ${fruits[0]}"
+echo "second fruit is: ${fruits[1]}"
+echo "third fruit is: ${fruits[2]}"
+echo "fourth fruit is: ${fruits[3]}
