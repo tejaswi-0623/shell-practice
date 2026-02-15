6ff3f44dbc361121f9689bb32a2f45841b2cc871
tejaswi <tejaswi0623@gmail.com>
2026-02-15 16:33:28 -0500

    msg

--- 08.datatypes.sh
+++ 08.datatypes.sh
@@ -25,4 +25,4 @@ echo "Fruits we defined are: ${fruits[@]}"
 echo "first fruit is: ${fruits[0]}"
 echo "second fruit is: ${fruits[1]}"
 echo "third fruit is: ${fruits[2]}"
-echo "fourth fruit is: ${fruits[3]}
+echo "fourth fruit is: ${fruits[3]}"
