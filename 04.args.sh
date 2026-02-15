18da58bd5e866b5adc38077e2d3090344566c2ee
tejaswi <tejaswi0623@gmail.com>
2026-02-15 12:27:13 -0500

    mesage

--- 04.args.sh
+++ 04.args.sh
@@ -5,5 +5,5 @@ person2=$2
 
 echo "$person1 :: Hi $person2, how are you?"
 echo "$person2 :: hi $person1, doing good. what about you?"
-echo "$person1 :: i am also doing fine $person2, "what are you learning today""
+echo "$person1 :: i am also doing fine $person2, what are you learning today?"
 echo "$person2 :: " i am learning something today $person1
