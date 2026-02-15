b8d4e00ec98c193f963c43ce8d9f9f584f81d04c
tejaswi <tejaswi0623@gmail.com>
2026-02-15 15:56:16 -0500

    message

---
+++ 07.specialvairables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###special vairables####
+#$1,$2.....$N are arguments passed to script
+echo "all arguments passed to script: $@"
+echo "number of arguments passed to script: $#"
+echo "to know script name: $0"
+echo "present working directory: $PWD"
+echo "who is running the script: $USER"
+echo "process intsance id (PID) of the script: $$"
+sleep 10
+echo "process instance id (PID) of recenelt excuted background process: $!"
+echo "exit status of previous command: $?"
