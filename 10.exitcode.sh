25906a93c1441ac5b54cf54266a3961eea6ca7bd
tejaswi <tejaswi0623@gmail.com>
2026-02-15 17:15:45 -0500

    message

---
+++ 10.exitcode.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+userid=$(id -u)
+
+if [ $userid ne 0 ]; then
+  echo "please give root access to user and run the script"
+  exit 1
+fi
+
+echo "installing nginx"
+dnf install nginx -y
+
+if [ $? equal 0 ]; then
+ echo "nginx installation is success"
+else
+ echo "nginx installation is failure"
+
+fi
