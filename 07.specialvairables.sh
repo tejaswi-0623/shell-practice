6e450dad78c0fd4339963db4734bc1ed13f7292a
tejaswi <tejaswi0623@gmail.com>
2026-02-15 16:02:35 -0500

    mesage

--- 07.specialvairables.sh
+++ 07.specialvairables.sh
@@ -11,3 +11,15 @@ echo "process intsance id (PID) of the script: $$"
 sleep 10 &
 echo "process instance id (PID) of recently excuted background process: $!"
 echo "exit status of previous command: $?"
+
+
+
+#sh 07.specialvairables.sh mani hani rani
+#all arguments passed to script: mani hani rani
+#number of arguments passed to script: 3
+#to know script name: 07.specialvairables.sh
+#present working directory: /home/ec2-user/shell-practice
+#who is running the script: ec2-user
+#process intsance id (PID) of the script: 15661
+#process instance id (PID) of recently excuted background process: 15662
+#exit status of previous command: 0
