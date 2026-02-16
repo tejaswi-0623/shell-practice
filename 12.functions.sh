7d7e8694c3c96a285d1196a1d07c16de56fbbdd7
tejaswi <tejaswi0623@gmail.com>
2026-02-16 16:12:49 -0500

    action

--- 12.functions.sh
+++ 12.functions.sh
@@ -9,20 +9,25 @@ fi
 
 validate(){
     if [ $1 -ne 0 ]; then
-     echo "$2 is.....success"
-    else
      echo "$2 is.....failed"
+    else
+     echo "$2 is.....success"
     fi
 }
 
 dnf install nginx -y
-validate $? "nginx installation"
+validate $? "nginx installation"  #here $1=$? and $2=nginx installtion
 
 dnf install mysql -y
-validate $? "mysql installation"
+validate $? "mysql installation"  #here $1=$? and $2=mysql installtion
 
 dnf install redis -y
-validate $? "redis installation"
+validate $? "redis installation" #here $1=$? and $2=redis installtion
 
+
+#here we didnot declare vairables separately shell automatically assigned vairables by itself
+#as $1--->$? and $2-->given installations
+#if you run the script as sh 12.functions.sh we get output as "please run this script with root user access"
+#if you run the script as sudo sh 12.functions.sh then nginx,mysql and nodejs will be installed and get as success
 
 
