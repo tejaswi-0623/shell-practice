e1fbecc06b9955f7c326f9ef6a5fb3ee159ff21c
tejaswi <tejaswi0623@gmail.com>
2026-02-17 16:13:25 -0500

    message

---
+++ 16.colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bish
+
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+blue="\e[34m"
+normal="\e[0m"
+
+echo -e "$red hello, $green how are you? $blue vanitha "
+echo -e "$green what are you learning today $normal may i know"
