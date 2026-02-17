2ae2c525536f76b407d279e29c87ed919cdbfd63
tejaswi <tejaswi0623@gmail.com>
2026-02-17 16:58:52 -0500

    message

--- 19.trap.sh
+++ 19.trap.sh
@@ -8,3 +8,10 @@ echo "Hello World"
 echo "I am learning Shell"
 echoo "printing error here"
 echo "No error in this"
+
+#run the script sh 19.trap.sh and we get output as below
+#Hello World
+#I am learning Shell
+#19.trap.sh: line 9: echoo: command not found
+#There is an error in 9, Command: echoo "printing error here"
+
