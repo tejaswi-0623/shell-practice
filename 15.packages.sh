76e2d8b9769f34cd425520e8a331fc5f10cc99d5
tejaswi <tejaswi0623@gmail.com>
2026-02-17 15:34:35 -0500

    message

---
+++ 15.packages.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+userid=$(id -u)
+logs_folder="var/log/shell-practice"
+logs_file="var/log/shell-practice/$0.log"
+
+if [ $userid -ne 0 ]; then
+ echo "please run the script with root user access" | tee -a $logs_file
+ exit 1
+fi
+
+mkdir -p $logs_folder
+
+validate(){
+    if [ $1 -ne 0 ]; then
+     echo "$2 installation is.......failed" | tee -a $logs_file
+     exit 1
+    else
+     echo "$2 installation is ......success" | tee -a $logs_file
+}
+
+for package in $@ #sudo give package names at run time
+do 
+ dnf list installed $package &>> $logs_file
+if [ $? -ne 0 ]; then
+ echo "$package not installed, installing now" &>> $logs_file
+ validate $? "$package"
+else
+  echo "$pacakge already installed, so skipping now"
+fi
+
+done
