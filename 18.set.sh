687e027edeb32cff92eff011cf848fd4d430812f
tejaswi <tejaswi0623@gmail.com>
2026-02-17 16:48:50 -0500

    message

--- 18.set.sh
+++ 18.set.sh
@@ -37,3 +37,15 @@ for package in $@  #give package names at run time with sudo access
       echo -e "$package already installed, $yellow skipping now $normal"
     fi
 done
+
+
+#if we give set -e no need to call validate function
+#if we give two packages like this sudo sh 18.set.sh nginx hkdhfid in run time we got output like below
+#nginx already installed,  skipping now
+#we didn't get any output or error msg in terminal but if you check in logs like below we observe error
+#sudo less var/log/shell-practice/18.set.sh.log
+#Installed Packages
+#nginx.x86_64           2:1.20.1-22.el9_6.3           @rhel-9-appstream-rhui-rpms
+#Error: No matching Packages to list
+#var/log/shell-practice/18.set.sh.log (END)
+#that is second package is wrong and exited the script
