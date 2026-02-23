b42783bafce6d0e59d5c1e5c1706ae41482bd6d9
tejaswi <tejaswi0623@gmail.com>
2026-02-23 17:45:11 -0500

    msg

---
+++ 21.script-1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+country="INDIA"
+code="+91"
+
+echo "I am living in $country"
+echo "code of $country to phone call is $code"
+echo "PID of script-1 is $$"
+
+sh 22.script-2.sh
---
+++ 22.script-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "I am living in $country"
+echo "code of the country to phone call is $code"
+echo "PID of script-1 is $$" 
