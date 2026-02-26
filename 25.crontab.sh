89fec7eac37ab6520bd52935bb661ec2091f8fe2
tejaswi <tejaswi0623@gmail.com>
2026-02-26 17:34:22 -0500

    msg

---
+++ 25.crontab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+logs_folder="/home/ec2-user/app-logs"
+logs_file="$logs_folder/$0.log"
+
+if [ ! -d $logs_folder ]; then
+  echo -e "$logs_folder does not exist"
+  exit 1
+fi
+
+files_to_delete=$(find $logs_folder -name "*.log" -mtime +14) #vairable=$(command)
+
+
+while IFS= read -r $file; do
+  echo -e "deleteing files : $logs_folder"
+  rm -f $file
+  echo -e "deleted files: $file"
+done <<< $files_to_delete 
