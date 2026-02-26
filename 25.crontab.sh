943fb4eabef5f51cc30fc8abe9146d0b62a276c3
tejaswi <tejaswi0623@gmail.com>
2026-02-26 17:54:52 -0500

    msg

--- 25.crontab.sh
+++ 25.crontab.sh
@@ -16,3 +16,10 @@ while IFS= read -r file; do
   rm -f $file
   echo  "deleted files: $file"
 done <<< $files_to_delete 
+
+#to schedule time to delete log for example to delte now
+#crontab -e edit crontab file like this to every minute ==>* * * * * sh /home/ec2-user/shell-practice/25.crontab.sh
+#save the crontab file
+#sudo tail -f /var/log/cron now we will see the shell-practice/25.crontab.sh deleting files
+#now cd and go to cd app-logs and give list ls -l the log files older than 14 days will be deleted
+#like this we can write script to delete log files and delete them by scheduling crontabs
