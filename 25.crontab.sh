e60cb328c03cff85366a04f11d186ebb5a698dd8
tejaswi <tejaswi0623@gmail.com>
2026-02-26 18:04:24 -0500

    msg

--- 25.crontab.sh
+++ 25.crontab.sh
@@ -12,7 +12,7 @@ files_to_delete=$(find $logs_folder -name "*.log" -mtime +14) #vairable=$(comman
 
 
 while IFS= read -r file; do
-  echo  "deleteing files : $logs_folder"
+  echo  "deleteing files are: $file"
   rm -f $file
   echo  "deleted files: $file"
 done <<< $files_to_delete 
@@ -23,3 +23,4 @@ done <<< $files_to_delete
 #sudo tail -f /var/log/cron now we will see the shell-practice/25.crontab.sh deleting files
 #now cd and go to cd app-logs and give list ls -l the log files older than 14 days will be deleted
 #like this we can write script to delete log files and delete them by scheduling crontabs
+#mkdir app-logs and create some files with older than 14 days and then execute this script
