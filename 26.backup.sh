dfd59e8ebe2567e242a621b383eea1140b91aac7
tejaswi <tejaswi0623@gmail.com>
2026-03-02 16:50:24 -0500

    backup

--- 26.backup.sh
+++ 26.backup.sh
@@ -13,7 +13,7 @@ days=${3:-14} #$3 for third argument and default we give as 14 days if user not 
 
 mkdir -p $logs_folder
 
-#cehcking root user or not
+#checking root user or not
 if [ $userid -ne 0 ]; then
   echo -e " $R please run the script with root user access $N"
   exit 1
@@ -32,3 +32,8 @@ usage(){
     log "$G usage sudo backup <source_dir> <destination_dir> <days>[Default 14 days] $N"
     exit 1
 }
+
+#If the user did not provide at least two arguments, call usage function and exit the script
+if [ $# -lt 2 ]; then #$#---number of arguments
+   usage
+fi
