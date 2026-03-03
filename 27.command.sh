0af5982dc29bad43321d7df9d6deb2c3b961588f
tejaswi <tejaswi0623@gmail.com>
2026-03-03 16:10:09 -0500

    logs

--- 27.command.sh
+++ 27.command.sh
@@ -7,5 +7,5 @@ echo  "giving script as command in linux"
 #sudo cp 27.commmand.sh /usr/bin/linuxcommand #copy this script into/usr/bin path and linuxcommand is just name
 #sudo chmod +x /usr/bin/linuxcommand --> giving execute permission
 #if we run command as sudo linuxcommand
-#we get output as hi from command
+#we get output in teminal as  hi from command
         #giving script as command in linux
