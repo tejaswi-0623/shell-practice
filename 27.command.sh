206aa099ae08f5681ac4da326aeae33b2199668d
tejaswi <tejaswi0623@gmail.com>
2026-03-03 16:04:33 -0500

    logs

--- 27.command.sh
+++ 27.command.sh
@@ -2,3 +2,10 @@
 
 echo  "hi from command"
 echo  "giving script as command in linux"
+
+#running this script as command
+#sudo cp 27.commmand.sh /usr/bin/linuxcommand #copy this script into/usr/bin path and linuxcommand is just name
+#sudo chmod +x /usr/bin/linuxcommand --> giving execute permission
+#if we run command as sudo linuxcommand
+#we get output as hi from command
+        #giving script as command in linux
